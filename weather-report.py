a3a0a5f6d3d065b13dcd0e97e70950609e6d9e02
Chi T. Vo <vochitrunggggg@gmail.com>
2025-10-02 16:36:01 -0500

    response for url_2 + putting the information into a json file for response_2

--- weather-report.py
+++ weather-report.py
@@ -7,13 +7,13 @@ params_1 = {"name": "Chicago", "country": "US", "count": 1, "format": "json"}
 response_1 = requests.get(URL_1, params=params_1)
 print("Status:", response_1.status_code)
 print("Final URL:", response_1.url)
-
 geo = response_1.json()["results"][0]
 lat, lon = geo["latitude"], geo["longitude"]
+
 URL_2 = "https://api.open-meteo.com/v1/forecast?current_weather=true"
 params_2 = {"latitude": lat, "longitude": lon, "current_weather": "true"}
 
-response_2 = requests.get(URL_2)
+response_2 = requests.get(URL_2, params=params_2)
 print("Status:", response_2.status_code)
 print("Final URL:", response_2.url)
 
